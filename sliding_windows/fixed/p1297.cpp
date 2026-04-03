c18c1424d718677bc4833b91076507b3950c9d96
youarer1ght <a.poe.y@qq.com>
2026-04-03 15:24:40 +0800

    p1297, p1652

---
+++ sliding_windows/fixed/p1297.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
+        unordered_map<string, int> re;  // 记录符合条件的 string
+        for(int len = minSize; len <= maxSize; len++){
+            unordered_map<char, int> count; // 计数 letters
+            // init
+            string curr = s.substr(0, len);
+            for(char c: curr){
+                count[c]++;
+            }
+            if(count.size() <= maxLetters){
+                re[curr]++;
+            }
+            // loop
+            for(int i = len; i < s.size(); i++){
+                // pop
+                count[s[i - len]]--;
+                if(count[s[i - len]] == 0){
+                    count.erase(s[i - len]);
+                }
+                // push
+                count[s[i]]++;
+                // check
+                curr = s.substr(i - len + 1, len);  // 注意+1
+                if(count.size() <= maxLetters){
+                    re[curr]++;
+                }
+            }
+        }
+        int maxCount = 0;
+        for(pair<string, int> p: re){
+            maxCount = max(maxCount, p.second);
+        }
+        return maxCount;
+    }
+};
--- sliding_windows/fixed/p1652.cpp
+++ sliding_windows/fixed/p1652.cpp
@@ -1,32 +1,47 @@
 #include <vector>
+#include <cmath>
 using namespace std;
 class Solution {
-private:
-    int init(vector<int>& code, int k, int& front, int& back){
-        int n = code.size();
-        int loop;
-        if(k > 0){
-            back = 1;
-            front = (k + 1) % n;
-            loop = k / n;
-        } else {
-            front = 0;
-            back = n - (-k) % n;
-            loop = (-k) % n;
-        }
-        int sumCode = 0;
-        for(int i = 0; i < n; i++){
-            sumCode += code[i];
-        }
-    }
 public:
     vector<int> decrypt(vector<int>& code, int k) {
-        vector<int> result(code.size(), 0);
-        if(k == 0){
-            return result;
+        int n = code.size();
+        vector<int> res(n, 0);
+        
+        if (k == 0) return res;
+        
+        int m = abs(k);
+        
+        if (k > 0) {
+            // 计算第一个窗口
+            int sum = 0;
+            for (int i = 1; i <= m; i++) {
+                sum += code[i % n];
+            }
+            res[0] = sum;
+            
+            // 滑动窗口
+            for (int i = 1; i < n; i++) {
+                sum -= code[i];
+                sum += code[(i + m) % n];
+                res[i] = sum;
+            }
+        } else {
+            // k < 0
+            // 计算第一个窗口：前 m 个元素（循环意义上）
+            int sum = 0;
+            for (int i = n - m; i < n; i++) {
+                sum += code[i];
+            }
+            res[0] = sum;
+            
+            // 滑动窗口
+            for (int i = 1; i < n; i++) {
+                sum -= code[(i - 1 - m + n) % n];
+                sum += code[i - 1];
+                res[i] = sum;
+            }
         }
-        // init
-        int sum = 0;
-        for(int i = 0; i < k; i++){}
+        
+        return res;
     }
 };
